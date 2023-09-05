4bde5f41007a1e2544138447ddd0968bb0b8fc7f
Assa <143470757+Assa228@users.noreply.github.com>
2023-09-05 18:36:44 +0200

    Update stegsnowbruteforcer.py

--- stegsnowbruteforcer.py
+++ stegsnowbruteforcer.py
@@ -1,8 +1,10 @@
-import argparse
 import subprocess
 import re
-from concurrent.futures import ThreadPoolExecutor
+import queue
+import threading
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+
 
 # ANSI color codes
 RED = '\033[91m'
@@ -23,7 +25,7 @@ signature = """
 print(signature)
 
 def try_password(file_path, password, output_file, keyword=None):
-    command = f'stegsnow -C -p {password} {file_path}'
+    command = f'stegsnow -C -p "{password}" {file_path}'
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='latin-1')
 
     if keyword and re.search(keyword, result.stdout):
@@ -48,12 +50,23 @@ def try_password(file_path, password, output_file, keyword=None):
     # Update the progress bar when the task is completed
     pbar.update(1)
 
+# Define a function to process passwords from the queue
+def process_passwords(file_path, output_file, keyword=None):
+    while True:
+        try:
+            password = password_queue.get(timeout=1)  # Wait for a password, timeout to check if we should exit
+        except queue.Empty:
+            break  # No more passwords, exit the thread
+        
+        try_password(file_path, password, output_file, keyword)
+        password_queue.task_done()
+
 parser = argparse.ArgumentParser(description='Brute-Force Attack Script for Stegsnow')
 parser.add_argument('--file', required=True, help='Path to the target file')
 parser.add_argument('--wordlist', required=True, help='Path to the password file')
 parser.add_argument('--output', required=True, help='Path to the output results file')
 parser.add_argument('--keyword', help='Keyword to search in the results')
-parser.add_argument('--threads', type=int, default=10, help='Number of threads (default: 10)')
+parser.add_argument('--threads', type=int, default=2, help='Number of threads (default: 2)')
 args = parser.parse_args()
 
 file_path = args.file
@@ -65,19 +78,31 @@ keyword = args.keyword
 with open(password_file, 'rb') as f:
     lines = f.readlines()
 
-num_threads = args.threads  # Use the value specified by the user or the default value (10)
+num_threads = args.threads  # Use the value specified by the user or the default value (2)
 total_passwords = len(lines)
 start_index = int(0.00 * total_passwords)  # progress percentage
 
 # Using the tqdm library to display real progress
 with ThreadPoolExecutor(max_workers=num_threads) as executor:
     with tqdm(total=total_passwords - start_index) as pbar:
+        # Create a queue for passwords
+        password_queue = queue.Queue()
         for line in lines[start_index:]:
             try:
                 password = line.decode('latin-1').strip()
             except UnicodeDecodeError:
                 continue
+            password_queue.put(password)
 
-            executor.submit(try_password, file_path, password, output_file, keyword)
+        # Start worker threads
+        threads = []
+        for _ in range(num_threads):
+            thread = threading.Thread(target=process_passwords, args=(file_path, output_file, keyword))
+            thread.start()
+            threads.append(thread)
+
+        # Wait for all worker threads to finish
+        for thread in threads:
+            thread.join()
 
 print("\nBrute-force attack completed. The results have been saved in", output_file)
